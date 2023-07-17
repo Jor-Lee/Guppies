e84d9f81b087c8d3de8c35740f072becfabd53f2
Jor-Lee <jordan.lee@physics.ox.ac.uk>
2023-07-17 18:44:24 +0100

    Combined truths and identified failed images
    Former-commit-id: 277cfe3d1bb02774e32ccc02c35a3b2ec0c64a85

--- Python_Files/helper.py
+++ Python_Files/helper.py
@@ -104,7 +104,7 @@ def ReadImage(img_byte_array, verbose=False):
     return output_string[:-1].upper(), word_confidences
 
 
-ReplaceSpecialCharacter = [['(', '1'], ['\\', '1'], ['+', 'G']]
+ReplaceSpecialCharacter = [['(', '1'], ['\\', '1'], ['+', 'G'], ['~~', 'W']]
 def RemoveSpecialCharacters(output_string, verbose=False):
     """Function removes all special characters that are read by the OCR."""
     if verbose: print("\nRemoving special characters from the output string (e.g. '.', '|').")
