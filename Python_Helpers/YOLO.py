2e4100e7e18d695b6e72f04b0b232849e6322d85
Jordan Lee <newc6448@ox.ac.uk>
2023-10-09 13:33:34 +0100

    YOLO preprocessing
    Former-commit-id: 2c215dd582540981377085d1b8a031a8f454ae57

--- Python_Helpers/YOLO.py
+++ Python_Helpers/YOLO.py
@@ -1,23 +1,84 @@
+import os
 import numpy as np
 import torch 
-from ultralytics import YOLO
+import tensorflow as tf
+from PIL import Image
+import matplotlib.pyplot as plt
 
-def UseYOLO(letter_model_path, number_model_path, image_file, probability_threshold=0.5, verbose=False):
+
+def UseYOLO(letter_model, number_model, ID_image, probability_threshold=0.5, verbose=False):
     relevant_characters =  {0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'B',10:'F',11:'G',12:'K',13:'N',14:'O',15:'P',16:'R',17:'S',18:'V',19:'W',20:'Y', 21:''}
-    
-    letter_model = YOLO(letter_model_path)
-    number_model = YOLO(number_model_path)
 
-    results_let = letter_model.predict(image_file)[0]
-    results_num = number_model.predict(image_file)[0]
+    processed_ID_image = prepare_image(ID_image)
+
+    file = './Temp_YOLO_File.jpg'
+
+    plt.imsave(file, processed_ID_image)
+
+    results_let = letter_model.predict(file)[0]
+    results_num = number_model.predict(file)[0]
+
+    os.remove('./Temp_YOLO_File.jpg')
 
     boxes, pred_vec, probs, letter_idx = analyse_results(results_let.boxes.data,results_num.boxes.data, probability_threshold)
     characters = []
     for vec in pred_vec:
         characters.append(relevant_characters[vec])
 
-    return characters, boxes, probs, letter_idx
+    return characters, boxes, probs, letter_idx, processed_ID_image
 
+
+def distortion_free_resize(image, img_size):
+    w, h = img_size
+    image = tf.image.resize(image, size=(h, w), preserve_aspect_ratio=True)
+
+    # Check tha amount of padding needed to be done.
+    pad_height = h - tf.shape(image)[0]
+    pad_width = w - tf.shape(image)[1]
+
+    # Only necessary if you want to do same amount of padding on both sides.
+    if pad_height % 2 != 0:
+        height = pad_height // 2
+        pad_height_top = height + 1
+        pad_height_bottom = height
+    else:
+        pad_height_top = pad_height_bottom = pad_height // 2
+
+    if pad_width % 2 != 0:
+        width = pad_width // 2
+        pad_width_left = width + 1
+        pad_width_right = width
+    else:
+        pad_width_left = pad_width_right = pad_width // 2
+
+    image = tf.pad(
+        image,
+        paddings=[
+            [pad_height_top, pad_height_bottom],
+            [pad_width_left, pad_width_right],
+            [0, 0],
+        ],
+    )
+
+    # image = tf.transpose(image, perm=[1, 0, 2])
+    # image = tf.image.flip_left_right(image)
+    return image
+
+def preprocess_image(image, img_size):
+    image = tf.convert_to_tensor(image)
+    image = distortion_free_resize(image, img_size)
+    image = tf.cast(image, tf.float32) / 255.0
+    return image
+
+
+def prepare_image(ID_image):
+    ID_image_width, ID_image_height = 256, 64
+    processed_ID_image = preprocess_image(ID_image[...,None],(ID_image_width,ID_image_height)).numpy()[None,...,0][0]
+    processed_ID_image = (((processed_ID_image - processed_ID_image.min()) / (processed_ID_image.max() - processed_ID_image.min())) * 255.9)
+    processed_ID_image = Image.fromarray(np.uint8(processed_ID_image))
+    processed_ID_image = processed_ID_image.convert("L")
+
+    return processed_ID_image
 
 def analyse_results(letter_data, number_data, prob_threshold):
 
@@ -42,7 +103,7 @@ def analyse_results(letter_data, number_data, prob_threshold):
 
     
     if data.shape[0] > 0:
-        #discard any characters that are too short?
+        # discard any characters that are too short?
         height_threshold = (data[:,3] - data[:,1]).max() * 0.5
         
         keep = data[:,3] - data[:,1] > height_threshold
